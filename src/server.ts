3f97aa53de0c4d4ba79990668b61445af6ea682e
javierspr <learn.javier@gmail.com>
2024-08-03 10:14:19 -0300

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import express from 'express'
 import router from './router'
+import db from './config/db'
+
+// Connect DB
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('connected')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB()
 
 const server = express()
 
