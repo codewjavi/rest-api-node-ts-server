934ad937f410c942e8c40fced0eb270b4a8f4d1b
javierspr <learn.javier@gmail.com>
2024-08-12 16:29:21 -0300

    add linking swagger to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import router from './router'
 import db from './config/db'
 import colors from 'colors'
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from './config/swagger'
 
 // Connect DB
 
@@ -26,9 +28,8 @@ server.use(express.json())
 
 server.use('/api/products', router)
 
-server.get('/api', (req, res) => {
-    res.json({msg: 'from API'})
-})
+// DOCS
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 export default server
 
