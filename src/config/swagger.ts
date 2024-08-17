c0ba7b0224ed3135bca61e6e5e0ee654335c7c83
javierspr <learn.javier@gmail.com>
2024-08-17 20:16:29 -0300

    add logo api

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,4 +1,5 @@
 import swaggerJSDoc from "swagger-jsdoc";
+import { SwaggerUiOptions } from "swagger-ui-express";
 
 const options: swaggerJSDoc.Options = {
     swaggerDefinition: {
@@ -11,8 +12,7 @@ const options: swaggerJSDoc.Options = {
         ],
         info: {
             title: 'REST API TypeScript | Node.js | Express',
-            version: '1.0.0',
-            description: 'API Docs for Products'
+            version: '1.0.0'
         }
     },
     apis: ['./src/router.ts']
@@ -20,4 +20,29 @@ const options: swaggerJSDoc.Options = {
 
 const swaggerSpec = swaggerJSDoc(options)
 
+const swaggerUiOptions: SwaggerUiOptions = {
+    customCss: `
+        .swagger-ui .topbar .topbar-wrapper {
+            display: block;
+        }
+        .topbar-wrapper .link {
+            content: url('https://media.licdn.com/dms/image/C5112AQF49DOfOhCFSA/article-cover_image-shrink_720_1280/0/1579816811751?e=2147483647&v=beta&t=e47GGJDzoqsm4dl3qV2EjVWrxyMzIwsPmEE9Gywo83w');
+            height: 60px;
+            width: auto;
+            border-radius: 30px;
+        }
+        .swagger-ui .topbar {
+            background-color: #fcfcfa;
+        }
+        .swagger-ui .info {
+            margin: 20px
+        }
+    `,
+    customSiteTitle: 'REST API Express | TypeScript DOCS'
+}
+
 export default swaggerSpec
+
+export {
+    swaggerUiOptions
+}
